93cbb885384c147392d46291f983048c93d2f5e6
RVA <aakash.sharma00004@gmail.com>
2025-02-19 09:37:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Aakash_Sharma",
+    author_email="aakash.sharma00004@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
